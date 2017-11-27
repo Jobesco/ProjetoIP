a2e20d6358828fba37eccf31c509097754e8ecc5
Jobesco <lucasjobrito@hotmail.com>
2017-11-27 18:32:27 -0200

    consertei um erro que deu no codigo,ultimas alteracoes nao codadas(mas as penultimas sim) - Job

--- bumberman/client/gameclient.c
+++ bumberman/client/gameclient.c
@@ -1,9 +1,6 @@
 #include "client.h"
 #include "default.h"
-<<<<<<< HEAD
 #include "comum.h"
-=======
->>>>>>> ed93abe8e799bec6ba0d3e9dbd8bee56ae728ea5
 #define max_clients 4
 #define tamanho_altura 8
 #define tamanho_largura 12
@@ -20,27 +17,17 @@ char posicao[2] = {""};
 char matriz[tamanho_altura][tamanho_largura] = {
     
     {pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra},
-<<<<<<< HEAD
     {pedra,verd_1,verd_1,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,verd_2,pedra},
     {pedra,verd_2,verd_1,verd_1,verd_1,verd_2,verd_1,verd_2,verd_1,verd_1,verd_1,pedra},
     {pedra,verd_1,verd_1,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,pedra},
     {pedra,verd_1,verd_1,verd_2,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,pedra},
     {pedra,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,pedra},
     {pedra,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_2,verd_1,pedra},
-=======
-    {pedra,verd_1,marrom,quebra,verd_2,verd_1,quebra,quebra,quebra,verd_1,verd_2,pedra},
-    {pedra,verd_2,verd_1,quebra,marrom,verd_2,quebra,verd_2,quebra,quebra,verd_1,pedra},
-    {pedra,quebra,quebra,marrom,verd_2,marrom,quebra,verd_1,marrom,quebra,quebra,pedra},
-    {pedra,quebra,quebra,verd_2,verd_1,verd_2,quebra,quebra,verd_1,marrom,quebra,pedra},
-    {pedra,verd_1,verd_2,quebra,quebra,marrom,marrom,verd_1,quebra,verd_1,quebra,pedra},
-    {pedra,verd_1,quebra,quebra,quebra,quebra,quebra,quebra,quebra,verd_2,verd_1,pedra},
->>>>>>> ed93abe8e799bec6ba0d3e9dbd8bee56ae728ea5
     {pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra}
     
 };
 
 typedef struct{
-<<<<<<< HEAD
   char id;
   char pos_x;
   char pos_y;
@@ -289,136 +276,6 @@ void main(){
             break;
         }
     }
-=======
-    
-    char id;
-    char pos_x;
-    char pos_y;
-    
-} jogador;
-
-typedef struct mensagem_cliente{
-    char pos_x;
-    char pos_y;
-    
-} msg_do_cliente;
-
-typedef struct p_broadcast{
-    
-    jogador jogadores[4]; //eh 2 pq recebe as posicoes(x,y) num plano cartesiano.
-    
-} msg_todos;
-
-msg_do_cliente minhapos;
-msg_todos basica;
-
-void tratar_intencao(char controle);
-
-void main(void){
-    
-    char *IP;
-    
-    IP = (char*)calloc(50,sizeof(char));
-    if(IP==NULL){
-        printf("ERRO NA ALOCACAO\n");
-        exit(1);
-    }
-    
-    int estado;
-    int tamanho_msg_entregue = 0;
-    int tamanho_msg_entregue_2 = 0;
-    int desconectado = 0;
-    int aux = 0;
-    char controle;
-    int retorno = 0;
-    
-    while(1){
-        
-        printf("Digite o IP onde deseja se conectar\n");
-        scanf(" %s",IP);
-        printf("Iremos logar com o IP %s\n", IP);
-        estado = connectToServer(IP);
-        
-        break;
-    }
-    
-    while(desconectado != 1){ // verifica se o client ainda joga
-        
-        if(estado == SERVER_UP){ //conexao estabelecida // prosseguir
-            int i = 0,j=0;
-            if(aux==0){
-                
-                aux++;
-                tamanho_msg_entregue = recvMsgFromServer(&minhapos,WAIT_FOR_IT); // manda
-                
-                for (i = 0; i<8; i++) {
-                    
-                    for (j=0;j<12; j++) {// imprime a matriz(job pediu, n sei p q!)
-                        
-                        printf("%2d",matriz[i][j]);
-                        
-                    }printf("\n");
-                    
-                }
-                
-            }
-            
-            if(tamanho_msg_entregue == sizeof(minhapos)){ // a mensagem foi recebida!
-                
-                for (i = 0; i<8; i++) {
-                    
-                    for (j=0;j<12; j++) {// imprime a matriz(job pediu, n sei p q!)
-                        
-                        if(minhapos.pos_x == i && minhapos.pos_y == j){
-                            printf("-1"); // valor p simbolizar o jogador
-                        }else{
-                            printf("%2d",matriz[i][j]);
-                        }
-                        
-                    }printf("\n");
-                    
-                }
-                
-                tamanho_msg_entregue_2 = recvMsgFromServer(&basica,DONT_WAIT);
-                
-                if(tamanho_msg_entregue_2 == sizeof(basica)){
-                    
-                    controle = getch();
-                    tratar_intencao(controle);
-                    retorno = sendMsgToServer(&minhapos,sizeof(msg_do_cliente)); // manda a intencao
-
-                    if(retorno == SERVER_DISCONNECTED){
-                        desconectado = 1;
-                    }
-                    
-                }
-                
-            }
-        }else if(estado == SERVER_DOWN){ //nao achou o server
-            
-            printf("Servidor nao encontrado!\n");
-            break;
-            
-        }else if(estado == SERVER_FULL){ // cheio
-            
-            printf("Servidor lotado! Aguarde proxima partida\n");
-            break;
-            
-        }else if(estado == SERVER_CLOSED){ // n aceita conexao
-            
-            printf("Servidor nao aceita novas conexoes!\n");
-            break;
-            
-        }else if(estado == SERVER_TIMEOUT){ // demorou p responder
-            
-            printf("Voce esperou demais, verifique sua conexao de dados!\n");
-            break;
-            
-        }
-        
-    }
-    
->>>>>>> ed93abe8e799bec6ba0d3e9dbd8bee56ae728ea5
 }
 
 int verifica_posix(int posix_x, int posix_y){
@@ -428,7 +285,6 @@ int verifica_posix(int posix_x, int posix_y){
     }else{
         return 0;
     }
-<<<<<<< HEAD
 }
 
 void tratar_intencao(char *controle){
@@ -469,53 +325,3 @@ void tratar_intencao(char *controle){
 			break;
 	}
 }
-=======
-    
-}
-
-void tratar_intencao(char controle){
-    
-    if(controle>96){
-        
-        controle -= 32;
-        
-    }
-    
-    switch(controle){
-            
-        case 'W':
-            
-            if(verifica_posix(minhapos.pos_x-1, minhapos.pos_y) == 1){
-                minhapos.pos_x -= 1;
-            }
-            
-            break;
-            
-        case 'A':
-            
-            if(verifica_posix(minhapos.pos_x, minhapos.pos_y-1) == 1){
-                minhapos.pos_y -= 1;
-            }
-            
-            break;
-            
-        case 'S':
-            
-            if(verifica_posix(minhapos.pos_x+1, minhapos.pos_y) == 1){
-                minhapos.pos_x += 1;
-            }
-            
-            break;
-            
-        case 'D':
-            
-            if(verifica_posix(minhapos.pos_x, minhapos.pos_y+1) == 1){
-                minhapos.pos_y += 1;
-            }
-           
-            break;
-
-    }
-    
-}
->>>>>>> ed93abe8e799bec6ba0d3e9dbd8bee56ae728ea5
