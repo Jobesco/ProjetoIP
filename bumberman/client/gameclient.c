85c2d10973843bf33605fff08142477c5681cadb
Lucas Job Brito de Araujo <ljba@ug1c22.windows.cin.ufpe.br>
2017-11-29 14:37:24 -0300

    modularizando umas besteiras..

--- bumberman/client/gameclient.c
+++ bumberman/client/gameclient.c
@@ -15,7 +15,7 @@ char posicao[2] = {""};
 
 
 char matriz[tamanho_altura][tamanho_largura] = {
-    
+
     {pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra},
     {pedra,verd_1,verd_1,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,verd_2,pedra},
     {pedra,verd_2,verd_1,verd_1,verd_1,verd_2,verd_1,verd_2,verd_1,verd_1,verd_1,pedra},
@@ -24,7 +24,7 @@ char matriz[tamanho_altura][tamanho_largura] = {
     {pedra,verd_1,verd_2,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,pedra},
     {pedra,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_1,verd_2,verd_1,pedra},
     {pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra,pedra}
-    
+
 };
 
 typedef struct{
@@ -35,7 +35,7 @@ typedef struct{
   char bomba; //se em algum momento,bomba for 1
   char posbomba_x;
   char posbomba_y;
-  
+
 } jogador;
 
 typedef struct mensagem_cliente{
@@ -47,9 +47,9 @@ typedef struct mensagem_cliente{
 } msg_do_cliente;
 
 typedef struct p_broadcast{
-  
+
   jogador jogadores[4]; //eh 2 pq recebe as posicoes(x,y) num plano cartesiano.
-  
+
 } msg_todos;
 
 
@@ -58,18 +58,19 @@ msg_todos basica;
 
 void tratar_intencao(char *controle);
 int verifica_posix(int posix_x, int posix_y);
+void printa_matriz();
 
 void main(){
 
 	char *IP;
-	
+
 	IP = (char*)calloc(50,sizeof(char));
 
 	if(IP==NULL){
 		printf("ERRO NA ALOCACAO\n");
 		exit(1);
 	}
-	
+
 	int estado;
 	int desconectado = 0;
 	int aux = 0,auxBomba = 0;
@@ -83,7 +84,7 @@ void main(){
     time_t inicioConexao,atualConexao; //para garantir q ele continue conectando
 
 	while(1){
-	
+
 		printf("Digite o IP onde deseja se conectar\n");
 
 		scanf(" %s",IP);
@@ -101,7 +102,7 @@ void main(){
 		}
 		break;
 	}
-	
+
 	while(desconectado != 1){ // verifica se o client ainda joga
 
         if(estado == SERVER_UP){ //conexao estabelecida // prosseguir
@@ -112,71 +113,19 @@ void main(){
             	printf("minha posicao eh %d - %d\n", minha_intencao.pos_x,minha_intencao.pos_y);
             	recvMsgFromServer(&basica,WAIT_FOR_IT);
 
-            	for(i=0;i<8;i++){
-                    for(j=0;j<12;j++){
-                    	verifica = 0;
-                    	for(k=0;k<4;k++){
-	                        if(basica.jogadores[k].pos_x == i && basica.jogadores[k].pos_y == j){
-	                            printf("%d",basica.jogadores[k].id+1); // valor p simbolizar o jogador
-	                            verifica++;
-	                        }
-	                    }
-	                    if(verifica==0){ //se ele n printou ngm,ele printa a matriz
-	                    	if(matriz[i][j] == 1 || matriz[i][j] == 2)
-	                    		printf("0");
-	                    	else
-	                    		printf("N");
-	                    }
-                    }printf("\n");     
-                }
+            	printa_matriz();
             }
 
            	tamanho_msg_entregue = recvMsgFromServer(&basica,DONT_WAIT);
 
             if(tamanho_msg_entregue != NO_MESSAGE){ // a mensagem foi recebida!
             	system("clear");
-                for(i=0;i<8;i++){
-                    for(j=0;j<12;j++){
-                    	verifica = 0;
-                    	for(k=0;k<4;k++){
-	                        if(basica.jogadores[k].pos_x == i && basica.jogadores[k].pos_y == j){
-	                            printf("%d",basica.jogadores[k].id+1); // valor p simbolizar o jogador
-	                            verifica++;
-	                        }else if(basica.jogadores[k].bomba == 1 && basica.jogadores[k].posbomba_x == i && basica.jogadores[k].posbomba_y == j){ //caso tenha uma bomba no mapa
-	                        	printf("b%d",basica.jogadores[k].id+1); //printa a bomba(mas o jogador vai em cima,caso esteja no mesmo bloco,por hora)
-	                        	verifica++;
-	                        }
-	                    }
-	                    if(verifica==0){ //se ele n printou ngm,ele printa a matriz
-	                    	if(matriz[i][j] == 1 || matriz[i][j] == 2)
-	                    		printf("0");
-	                    	else
-	                    		printf("N");
-	                    }
-                    }printf("\n");     
-                }
-            }
-
-            for(i=0;i<4;i++){ //loop referente a tratar a explosao
-            	if(basica.jogadores[k].bomba == 1){ //se existir uma bomba
-            		if(auxBomba == 0){
-            			inicioConexao = time(NULL);
-            			auxBomba++;
-            		}else{
-            			atualConexao = time(NULL);
-            			if(difftime(atualConexao,inicioConexao) >= 4){ //se a bomba estiver na hr de explodir (tempo sujeito a mudancas)
-            				printf("BOOM\n");
-
-            				auxBomba = 0;
-            			}
-            		}
-
-            	}
+                printa_matriz();
             }
 
             controle = getch();
 	        tratar_intencao(&controle);
-            
+
             if(controle != NO_KEY_PRESSED){ //se ele apertou uma tecla
             	if(controle != 'K'){
 	            	retorno = sendMsgToServer(&minha_intencao,sizeof(msg_do_cliente)); // manda a intencao
@@ -189,21 +138,21 @@ void main(){
             if(retorno == SERVER_DISCONNECTED){
 	        	desconectado = 1;
 	        }
-        
+
         }else if(estado == SERVER_DOWN){ //nao achou o server
-            
+
             printf("Servidor nao encontrado :S\n");
             break;
         }else if(estado == SERVER_FULL){ // cheio
-            
+
             printf("Servidor lotado!\nAguarde proxima partida :/\n");
             break;
         }else if(estado == SERVER_CLOSED){ // n aceita conexao
-            
+
             printf("Servidor nao aceita novas conexoes! >:U\n");
             break;
         }else if(estado == SERVER_TIMEOUT){ // demorou p responder
-            
+
             printf("Voce esperou demais, verifique sua conexao de dados! :P\n");
             break;
         }
@@ -211,7 +160,7 @@ void main(){
 }
 
 int verifica_posix(int posix_x, int posix_y){
-    
+
     if(matriz[posix_x][posix_y] == 1 || matriz[posix_x][posix_y] == 2){
         return 1;
     }else{
@@ -223,7 +172,7 @@ void tratar_intencao(char *controle){
 	if(controle[0]>96){
 		controle[0] -= 32;
 	}
-	
+
 	switch(controle[0]){
 		case 'W':
             if(verifica_posix(minha_intencao.pos_x-1, minha_intencao.pos_y) == 1){
@@ -231,21 +180,21 @@ void tratar_intencao(char *controle){
             }else
             	controle[0] = NO_KEY_PRESSED;
             break;
-            
+
         case 'A':
             if(verifica_posix(minha_intencao.pos_x, minha_intencao.pos_y-1) == 1){
                 minha_intencao.pos_y -= 1;
             }else
             	controle[0] = NO_KEY_PRESSED;
             break;
-            
+
         case 'S':
             if(verifica_posix(minha_intencao.pos_x+1, minha_intencao.pos_y) == 1){
                 minha_intencao.pos_x += 1;
             }else
             	controle[0] = NO_KEY_PRESSED;
             break;
-            
+
         case 'D':
             if(verifica_posix(minha_intencao.pos_x, minha_intencao.pos_y+1) == 1){
                 minha_intencao.pos_y += 1;
@@ -256,4 +205,27 @@ void tratar_intencao(char *controle){
 			minha_intencao.bomba = 1;
 			break;
 	}
+}
+
+void printa_matriz(){
+    for(i=0;i<tamanho_altura;i++){
+        for(j=0;j<tamanho_largura;j++){
+            verifica = 0;
+            for(k=0;k<4;k++){
+                if(basica.jogadores[k].pos_x == i && basica.jogadores[k].pos_y == j){
+                    printf("%d",basica.jogadores[k].id+1); // valor p simbolizar o jogador
+                    verifica++;
+                }else if(basica.jogadores[k].bomba == 1 && basica.jogadores[k].posbomba_x == i && basica.jogadores[k].posbomba_y == j){ //caso tenha uma bomba no mapa
+                    printf("b%d",basica.jogadores[k].id+1); //printa a bomba(mas o jogador vai em cima,caso esteja no mesmo bloco,por hora)
+                    verifica++;
+                }
+            }
+            if(verifica==0){ //se ele n printou ngm,ele printa a matriz
+                if(matriz[i][j] == 1 || matriz[i][j] == 2)
+                    printf("0");
+                else
+                    printf("N");
+            }
+        }printf("\n");
+    }
 }
